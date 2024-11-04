8163a3aafb77ca70e7a298306a8bd7a164640068
Göktuğ Kola <goktugkola@gmail.com>
2024-11-04 22:57:47 +0300

    Room types class
    Scriptible object of rooms

---
+++ Assets/Scripts/Rooms/Room.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+    public RoomTemplate roomTemplate;
+
+    public Resources resources;
+
+    public int roomid;
+
+    public float updateInterval = 5f;
+
+    private float timer;
+
+    void Start()
+    {
+        roomid = roomTemplate.id;
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= updateInterval)
+        {
+            UpdateResources();
+            timer = 0f;
+        }
+    }
+
+    void UpdateResources()
+    {
+        resources.goins += roomTemplate.goins * resources.satisfaction;
+        resources.energy += roomTemplate.energy * resources.satisfaction;
+        resources.alcohol += roomTemplate.alcohol * resources.satisfaction;
+        resources.coal += roomTemplate.coal * resources.satisfaction;
+        resources.dopamin += roomTemplate.dopamin * resources.satisfaction;
+
+        // Kaynakların minimum ve maksimum sınırlarını belirleyebiliriz
+        resources.goins = Mathf.Clamp(resources.goins, 0, 100000);
+        resources.energy = Mathf.Clamp(resources.energy, 0, 100000);
+        resources.alcohol = Mathf.Clamp(resources.alcohol, 0, 100000);
+        resources.coal = Mathf.Clamp(resources.coal, 0, 100000);
+        resources.dopamin = Mathf.Clamp(resources.dopamin, 0, 100000);
+    }
+}
