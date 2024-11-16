5de117ab180ef5b84d4493492469c40416865939
Zerda Ece UYĞUN <zerdaeceuygun@posta.mu.edu.tr>
2024-11-16 16:53:09 +0300

    Ozanın yaptıkları alındı
    Kilitli odalar için nesne koyuldu

--- Assets/Scripts/Rooms/UnlockingFloors.cs
+++ Assets/Scripts/Rooms/UnlockingFloors.cs
@@ -1,22 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
+//using System.Numerics;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class UnlockingFloors : MonoBehaviour
 {
-    public Resources resources;
+    // public Resources resources;
 
-    //public GameObject lockGameObject;test için
+    public GameObject lockGameObject;//test için
+    public GameObject newRooms;
     // Start is called before the first frame update
     [SerializeField] private int need;
+    
+    void Update()
+{
+    unlock();
+}    
+     void OnMouseOver()
+        {
+            gameObject.GetComponent<Renderer>().material.color = Color.grey;
+            if (Input.GetMouseButton(0))
+                {
+                   //ui açacak
+     
+               }
+        }
+        void OnMouseExit()
+        {
+            gameObject.GetComponent<Renderer>().material.color = Color.white;   
+        }
 
     void unlock()
     {
-        if (resources.goins >= need)
+       // if (resources.goins >= need)
+       if (Input.GetKeyDown(KeyCode.Space))//test için
         {
-            resources.goins -= need;
+           //resources.goins -= need;
+           
             Destroy(gameObject);
+            
         }
 
     }
+    
 }
