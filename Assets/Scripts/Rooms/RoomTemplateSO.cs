2f1d9ea9329d90506c251b38bb9edf369276135f
Göktuğ Kola <goktugkola@gmail.com>
2024-12-09 22:36:14 +0300

    An unnoticeable update to rooms scriptibleobject template

--- Assets/Scripts/Rooms/RoomTemplateSO.cs
+++ Assets/Scripts/Rooms/RoomTemplateSO.cs
@@ -6,11 +6,12 @@ public class RoomTemplate : ScriptableObject
 {
     public int id;
     public string roomType;
+    public string Description;
     [Header("Resources")]
     public int goins;
     public int energy;
     public int alcohol;
     public int coal;
     public int dopamin;
-
+    public List<RoomTemplate> RoomUpdates;
     }
