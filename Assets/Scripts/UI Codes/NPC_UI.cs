d64dc9c7d441e9953877c81edceb77485da9ef5e
Zerda Ece UYĞUN <zerdaeceuygun@posta.mu.edu.tr>
2025-01-10 22:39:45 +0300

    Npc relation panel

--- Assets/Scripts/UI Codes/NPC_UI.cs
+++ Assets/Scripts/UI Codes/NPC_UI.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NPC_UI : MonoBehaviour
 {
+    public GameObject PanelforNpc;
+    public bool isOpen ;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +17,18 @@ public class NPC_UI : MonoBehaviour
     void Update()
     {
         
+    }
+    public void ToggleOpen_Npc_Panels()
+    {
+     if ( !isOpen )
+        {
+            PanelforNpc.SetActive( true );
+            isOpen = true;
+        }
+        else if ( isOpen )
+        {
+            PanelforNpc.SetActive( false );
+            isOpen = false;
+        }
     }
 }
