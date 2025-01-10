ae9a1b3a6645df0b1da956ca688674171ec947eb
Zerda Ece UYĞUN <zerdaeceuygun@posta.mu.edu.tr>
2025-01-10 19:52:26 +0300

    uı panel opening closing

---
+++ Assets/Scripts/ForUI.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ForUI : MonoBehaviour
+{   
+    public Button GeneralInfoButton;
+    public Button RoomInfobutton;
+    public Button ShopButton;
+    public Button ResearchButton;
+    public GameObject GeneralInfo;
+    public GameObject RoomInfo;
+    public GameObject ShopInfo;
+    public GameObject Research;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+    }
+}
