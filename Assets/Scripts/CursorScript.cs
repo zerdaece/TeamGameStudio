0349e2119448cdb7b3135411928a15a50bb85eb0
CemTepe <metetepe7@gmail.com>
2025-01-10 19:05:58 +0300

    Cursss

--- Assets/Scripts/CursorScript.cs
+++ Assets/Scripts/CursorScript.cs
@@ -4,29 +4,15 @@ using UnityEngine;
 
 public class CursorScript : MonoBehaviour
 {
-    private Vector2 screenPoint;
+    public ParticleSystem cursorParticle;
+    void Update()
+    {
+        Vector3 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        cursorParticle.transform.position = new Vector3(cursorPos.x, cursorPos.y, 0f);
 
-        [SerializeField]
-        private GameObject particles;
-        private Vector2 mousePos;
-
-        private void Start()
+        if(Input.GetMouseButtonDown(0))
         {
-            particles.SetActive(false);    
-        }   
-
-        private void Update() 
-        {
-            if(Input.GetMouseButtonDown(0))
-            {
-                mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-                particles.SetActive(true);
-                particles.transform.position = new Vector3(mousePos.x, mousePos.y, 0f);
-            }
-
-            if(Input.GetMouseButtonUp(0))
-            {
-                particles.SetActive(false);
-            }
-        } 
+            cursorParticle.Play();
+        }
+    }
 }
