648f19b690a3407e6fef13c171fa0ab25f4873cf
Göktuğ Kola <goktugkola@gmail.com>
2025-01-11 06:44:15 +0300

    d

--- Assets/Scripts/ResearchSO.cs
+++ Assets/Scripts/ResearchSO.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+[CreateAssetMenu(fileName = "Research")]
 public class Research : ScriptableObject
 {
 public int goins;
